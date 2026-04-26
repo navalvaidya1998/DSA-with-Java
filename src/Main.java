33e850905feeb6204ea9d88e030a835f83d25b8e
Naval <navalvaidya1998@gmail.com>
2026-04-27 00:46:34 +0530

    Day 1: Two Sum

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 import arrays.LargestElement;
+import arrays.TwoSum;
 
 public class Main {
     public static void main(String[] args) {
-
+        //Sample example------------------------------------
         int[] arr = {3, 7, 2, 9, 5};
-
         int result = LargestElement.findLargest(arr);
-
         System.out.println("Largest Element: " + result);
+        //--------------------------------------------------
+        //Day 1: TwoSum
+        int[] arr1 = {2, 7, 11, 15};
+        int target = 26;
+        TwoSum twoSumSolver = new TwoSum();
+        int[] getTwoSumResult = twoSumSolver.twoSum(arr1, target);
+        System.out.println("Day 1 - TwoSum output ==> Indices: " + getTwoSumResult[0] + ", " + getTwoSumResult[1]);
     }
 }
--- src/arrays/LargestElement.java
+++ src/arrays/LargestElement.java
@@ -4,13 +4,11 @@ public class LargestElement {
 
     public static int findLargest(int[] arr) {
         int max = arr[0];
-
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] > max) {
                 max = arr[i];
             }
         }
-
         return max;
     }
 }
